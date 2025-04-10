fb51a111050280da2d0bf2ab4348e79136fb08ec
Victor Bugnion <victor@bugnion.org>
2025-04-10 16:16:47 +0200

    take into account atom type to index swap

--- analysis_part2_averaging.py
+++ analysis_part2_averaging.py
@@ -15,29 +15,47 @@ for root,subdir,files in os.walk("results_first_processing"):
         with h5py.File(output_filename,"w") as outfile :
 
             n_runs = sum([file.endswith(".h5") for file in os.listdir(root)])
-            crosses_00 = np.full((1000,n_runs),np.nan)
-            theprod_01 = np.full((1000,n_runs),np.nan)
-            r2s_00 = np.full((1000,n_runs),np.nan)
+            crosses_AA = np.full((1000,n_runs),np.nan)
+            theprod_AA = np.full((1000,n_runs),np.nan)
+            r2s_AA = np.full((1000,n_runs),np.nan)
             r2s_vacancy = np.full((1000,n_runs),np.nan)
-            theprod_00 = np.full((1000,n_runs),np.nan)
-            theprod_11 = np.full((1000,n_runs),np.nan)
-            r2s_11 = np.full((1000,n_runs),np.nan)
-            crosses_11 = np.full((1000,n_runs),np.nan)
+            theprod_BB = np.full((1000,n_runs),np.nan)
+            r2s_BB = np.full((1000,n_runs),np.nan)
+            crosses_BB = np.full((1000,n_runs),np.nan)
+            theprod_AB = np.full((1000,n_runs),np.nan)
             time_collector = np.full((1000,n_runs),np.nan)
+
 
             i = 0
             for file in files:
                 if file.endswith(".h5"):
                     input_filename = os.path.join(root,file)
                     with h5py.File(input_filename, "r") as f:
-                        crosses_00[:, i] = f["cross_00"][:]
-                        theprod_01[:, i] = f["theprod_01"][:]
-                        r2s_00[:, i] = f["r2s_00"][:]
-                        r2s_vacancy[:, i] = f["r2s_vacancy"][:]
-                        theprod_00[:, i] = f["theprod_00"][:]
-                        theprod_11[:, i] = f["theprod_11"][:]
-                        r2s_11[:, i] = f["r2s_11"][:]
-                        crosses_11[:, i] = f["cross_11"][:]
+                        if f["input"]["atom_key"]["Al"][()] == 0 and f["input"]["atom_key"]["Fe"][()] == 1 :
+                            crosses_AA[:, i] = f["cross_00"][:]
+                            theprod_AA[:, i] = f["theprod_00"][:]
+                            r2s_AA[:, i] = f["r2s_00"][:]
+
+                            r2s_vacancy[:, i] = f["r2s_vacancy"][:]
+                            theprod_AB[:, i] = f["theprod_01"][:]
+
+                            theprod_BB[:, i] = f["theprod_11"][:]
+                            r2s_BB[:, i] = f["r2s_11"][:]
+                            crosses_BB[:, i] = f["cross_11"][:]
+
+                        elif f["input"]["atom_key"]["Fe"][()] == 0 and f["input"]["atom_key"]["Al"][()] == 1 :
+
+                            crosses_BB[:, i] = f["cross_00"][:]
+                            theprod_BB[:, i] = f["theprod_00"][:]
+                            r2s_BB[:, i] = f["r2s_00"][:]
+
+                            r2s_vacancy[:, i] = f["r2s_vacancy"][:]
+                            theprod_AB[:, i] = f["theprod_01"][:]
+
+                            theprod_AA[:, i] = f["theprod_11"][:]
+                            r2s_AA[:, i] = f["r2s_11"][:]
+                            crosses_AA[:, i] = f["cross_11"][:]
+                        
                         time_collector[:, i] = f["time_collector"][:]
 
                         if i == 0 :
@@ -46,24 +64,24 @@ for root,subdir,files in os.walk("results_first_processing"):
                         i += 1
 
 
-            outfile.create_dataset("crosses_00", data=crosses_00.mean(axis=1))
-            outfile.create_dataset("theprod_01", data=theprod_01.mean(axis=1))
-            outfile.create_dataset("r2s_00", data=r2s_00.mean(axis=1))
+            outfile.create_dataset("crosses_AA", data=crosses_AA.mean(axis=1))
+            outfile.create_dataset("theprod_AB", data=theprod_AB.mean(axis=1))
+            outfile.create_dataset("r2s_AA", data=r2s_AA.mean(axis=1))
             outfile.create_dataset("r2s_vacancy", data=r2s_vacancy.mean(axis=1))
-            outfile.create_dataset("theprod_00", data=theprod_00.mean(axis=1))
-            outfile.create_dataset("theprod_11", data=theprod_11.mean(axis=1))
-            outfile.create_dataset("r2s_11", data=r2s_11.mean(axis=1))
-            outfile.create_dataset("crosses_11", data=crosses_11.mean(axis=1))
+            outfile.create_dataset("theprod_AA", data=theprod_AA.mean(axis=1))
+            outfile.create_dataset("theprod_BB", data=theprod_BB.mean(axis=1))
+            outfile.create_dataset("r2s_BB", data=r2s_BB.mean(axis=1))
+            outfile.create_dataset("crosses_BB", data=crosses_BB.mean(axis=1))
             outfile.create_dataset("time_collector", data=time_collector.mean(axis=1))
 
-            outfile.create_dataset("crosses_00_std", data=crosses_00.std(axis=1))
-            outfile.create_dataset("theprod_01_std", data=theprod_01.std(axis=1))
-            outfile.create_dataset("r2s_00_std", data=r2s_00.std(axis=1))
+            outfile.create_dataset("crosses_AA_std", data=crosses_AA.std(axis=1))
+            outfile.create_dataset("theprod_AB_std", data=theprod_AB.std(axis=1))
+            outfile.create_dataset("r2s_AA_std", data=r2s_AA.std(axis=1))
             outfile.create_dataset("r2s_vacancy_std", data=r2s_vacancy.std(axis=1))
-            outfile.create_dataset("theprod_00_std", data=theprod_00.std(axis=1))
-            outfile.create_dataset("theprod_11_std", data=theprod_11.std(axis=1))
-            outfile.create_dataset("r2s_11_std", data=r2s_11.std(axis=1))
-            outfile.create_dataset("crosses_11_std", data=crosses_11.std(axis=1))
+            outfile.create_dataset("theprod_AA_std", data=theprod_AA.std(axis=1))
+            outfile.create_dataset("theprod_BB_std", data=theprod_BB.std(axis=1))
+            outfile.create_dataset("r2s_BB_std", data=r2s_BB.std(axis=1))
+            outfile.create_dataset("crosses_BB_std", data=crosses_BB.std(axis=1))
             outfile.create_dataset("time_collector_std", data=time_collector.std(axis=1))
 
             outfile.create_dataset("n_runs", data=n_runs)
