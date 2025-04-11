08a524b62d4577ad1f3bc4d88006dd4a0a90555d
Victor Bugnion <victor@bugnion.org>
2025-04-11 10:58:57 +0200

    being nice

--- run_all_compositions.sh
+++ run_all_compositions.sh
@@ -6,5 +6,5 @@ mkdir -p results_first_processing/GA1_GB100_binary
 for input_file in inputs/GA1_GB100_binary/*
 do
     echo "Submitting job for $input_file"
-    sbatch single_composition.slurm "$input_file"
+    sbatch --nice=10000 single_composition.slurm "$input_file"
 done
