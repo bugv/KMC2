d8046bd4e3e416a66809f342bad51cd792dca84e
Victor Bugnion <victor@bugnion.org>
2025-04-17 00:03:10 +0200

    bash issues

--- scripts/run_ternary_grid_ideal.sh
+++ scripts/run_ternary_grid_ideal.sh
@@ -6,5 +6,5 @@ mkdir -p results_first_processing/ideal_ternary
 for input_file in inputs/ideal_ternary/*
 do
     echo "Submitting job for $input_file"
-    sbatch scripts/single_composition.slurm "ideal_ternary/$input_file"
+    sbatch scripts/single_composition.slurm "$input_file"
 done
